4fc767abcf36267f7d8f34f0ced335465d6bbe7e
data-kid <gabriel.grover@gmail.com>
2018-04-15 12:30:15 -0700

    game state machine implemented with tests

--- lib/tic_tac_toe/game_state_m.ex
+++ lib/tic_tac_toe/game_state_m.ex
@@ -5,10 +5,29 @@ defmodule TicTacToe.GameStateM do
   def new, do: %GameStateM{}
 
   def check(%GameStateM{state: :initialized} = game_state, :add_player), do:
-    %GameStateM{game_state | state: :player_ready}
+    {:ok, %GameStateM{game_state | state: :player1_turn}}
 
   def check(%GameStateM{state: :initialized} = game_state, :add_cpu_player), do:
-    %GameStateM{game_state | state: :cpu_ready}
+    {:ok, %GameStateM{game_state | state: :cpu1_turn}}
+
+  def check(%GameStateM{state: :player1_turn} = game_state, {:mark, :player1}), do:
+    {:ok, %GameStateM{game_state | state: :player2_turn}}
+
+  def check(%GameStateM{state: :player2_turn} = game_state, {:mark, :player2}), do:
+    {:ok, %GameStateM{game_state | state: :player1_turn}}
+
+  def check(%GameStateM{state: :player1_turn} = game_state, {:chk_win, :win}), do:
+    {:ok, %GameStateM{game_state | state: :game_over}}
+
+  def check(%GameStateM{state: :player1_turn} = game_state, {:chk_win, :no_win}), do:
+    {:ok, game_state}
+
+  def check(%GameStateM{state: :player2_turn} = game_state, {:chk_win, :win}), do:
+    {:ok, %GameStateM{game_state | state: :game_over}}
+
+  def check(%GameStateM{state: :player2_turn} = game_state, {:chk_win, :no_win}), do:
+    {:ok, game_state}
+
 
   def check(_state, _action), do: :error
 end
--- test/game_state_m_test.exs
+++ test/game_state_m_test.exs
@@ -13,8 +13,8 @@ defmodule GameStateMTest do
       {:ok, %{fresh_state: GameStateM.new}}
     end
 
-    test "should change game state to :player_ready", %{fresh_state: state} do
-      assert %GameStateM{state: :player_ready} = GameStateM.check(state, :add_player)
+    test "should change game state to :player1_turn", %{fresh_state: state} do
+      assert {:ok, %GameStateM{state: :player1_turn}} = GameStateM.check(state, :add_player)
     end
 
     test ":add_player action should yield error if state was not in :initialized" do
@@ -22,8 +22,8 @@ defmodule GameStateMTest do
       assert :error = GameStateM.check(game_state, :add_player)
     end
 
-    test "should change game state to :cpu_ready", %{fresh_state: state} do
-      assert %GameStateM{state: :cpu_ready} = GameStateM.check(state , :add_cpu_player)
+    test "should change game state to :cpu1_turn", %{fresh_state: state} do
+      assert {:ok, %GameStateM{state: :cpu1_turn}} = GameStateM.check(state , :add_cpu_player)
     end
 
     test ":add_cpu_player action should yield :error if state was not in :initialized" do
@@ -33,6 +33,42 @@ defmodule GameStateMTest do
 
     test "should return error on unrecognized action", %{fresh_state: state} do
       assert :error = GameStateM.check(state, :invalid_action)
+    end
+
+    test "should change game state to :player2_turn when player1 makes a mark that does not win" do
+      game_state = %GameStateM{state: :player1_turn}
+      assert {:ok, %GameStateM{state: :player2_turn}} = GameStateM.check(game_state, {:mark, :player1})
+    end
+
+    test "action {:mark, :player1} should yield error if state was not in :player1_turn" do
+      game_state = %GameStateM{state: :player2_turn}
+      assert :error = GameStateM.check(game_state, {:mark, :player1})
+    end
+
+    test "should change game state to :player1_turn when player2 makes a mark that does not win" do
+      game_state = %GameStateM{state: :player2_turn}
+      assert {:ok, %GameStateM{state: :player1_turn}} = GameStateM.check(game_state, {:mark, :player2})
+    end
+
+    test "action {:mark, :player2} should yield error if state was not in :player2_turn" do
+      game_state = %GameStateM{state: :player1_turn}
+      assert :error = GameStateM.check(game_state, {:mark, :player2})
+    end
+
+    test "action {:chk_win, has_won} should change state to :game_over if has_won is :win" do
+      game_state = %GameStateM{state: :player1_turn}
+      assert {:ok, %GameStateM{state: :game_over}} = GameStateM.check(game_state, {:chk_win, :win})
+
+      game_state = %GameStateM{state: :player2_turn}
+      assert {:ok, %GameStateM{state: :game_over}} = GameStateM.check(game_state, {:chk_win, :win})
+    end
+
+    test "action {:chk_win, has_won} should not change state if has_won is :no_win" do
+      game_state = %GameStateM{state: :player1_turn}
+      assert {:ok, %GameStateM{state: :player1_turn}} = GameStateM.check(game_state, {:chk_win, :no_win})
+
+      game_state = %GameStateM{state: :player2_turn}
+      assert {:ok, %GameStateM{state: :player2_turn}} = GameStateM.check(game_state, {:chk_win, :no_win})
     end
   end
 end

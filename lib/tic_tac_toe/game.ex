3023c0b473e26dfacd7010e5cd16e6ac06d006d9
data-kid <gabriel.grover@gmail.com>
2018-04-15 20:33:45 -0700

    initial commit for this branch.  we are taking out the board size argument and replacing it with a list of winnning sets for the argument

--- lib/tic_tac_toe/game.ex
+++ lib/tic_tac_toe/game.ex
@@ -1,10 +1,25 @@
 defmodule TicTacToe.Game do
-  alias __MODULE__
+  alias TicTacToe.{Board, Game, WinningSets, GameStateM}
+  
+  defstruct board: nil, winning_sets: nil, state_machine: nil
 
-  # defstruct [:board, :winning_sets, :game_state]
-  defstruct board: nil, winning_sets: nil, game_state: nil
-
-  def start_link do
-    Agent.start_link(fn -> %Game{} end)
+  def new(board_size) do
+    {:ok, board} = Board.new(board_size)
+    {
+      :ok,
+      %Game{
+        board: board,
+        winning_sets: WinningSets.build(board_size),
+        state_machine: GameStateM.new()
+      }
+    }
   end
+
+  def start_link(board_size) do
+    Agent.start_link(fn ->
+      {:ok, game} = new(board_size)
+      game
+    end)
+  end
+
 end
--- test/game_test.exs
+++ test/game_test.exs
@@ -1,15 +1,28 @@
 defmodule GameTest do
   use ExUnit.Case
-  alias TicTacToe.{Game, Board, GameStateM, winning_sets}
+  alias TicTacToe.{Game, Board, GameStateM, WinningSets}
 
   describe "start_link/0" do
+    setup  do
+      {:ok, %{game_agent: Game.start_link(3)}}
+    end
     test "should start an agent with correct state" do
-      assert {:ok, game_pid} = Game.start_link()
+      {:ok, expected_board} = Board.new(3)
+      expected_state_m = GameStateM.new()
+      expected_winning_sets = WinningSets.build(3)
+      assert {:ok, game_pid} = Game.start_link(3)
       assert %Game{
         board: %Board{} = received_board,
         winning_sets: received_winning_sets,
-        game_state: %GameStateM{} = received_game_state
+        state_machine: %GameStateM{} = received_state_m
       } = Agent.get(game_pid, fn state -> state end)
+
+      assert expected_board == received_board
+      assert expected_state_m == received_state_m
+      assert expected_winning_sets == received_winning_sets
+    end
+
+    test "" do
 
     end
   end

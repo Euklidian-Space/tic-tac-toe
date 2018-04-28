ee972bd7f58dfc615310d44998efa6a8381a83e3
data-kid <gabriel.grover@gmail.com>
2018-04-28 09:41:17 -0700

    cleaned up tic_tac_toe.ex

--- lib/tic_tac_toe.ex
+++ lib/tic_tac_toe.ex
@@ -3,16 +3,4 @@ defmodule TicTacToe do
   Documentation for TicTacToe.
   """
 
-  @doc """
-  Hello world.
-
-  ## Examples
-
-      iex> TicTacToe.hello
-      :world
-
-  """
-  def hello do
-    :world
-  end
 end

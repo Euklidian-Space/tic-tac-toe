3096edc5cbea127e4964928f9b16e8e559147869
data-kid <gabriel.grover@gmail.com>
2018-05-01 21:03:39 -0700

    initial commit for branch

--- lib/tic_tac_toe/graphics/x_marker.ex
+++ lib/tic_tac_toe/graphics/x_marker.ex
@@ -1,5 +1,8 @@
 defmodule TicTacToe.TextGraphics.XMarker do
+  alias TicTacToe.Board
   alias TicTacToe.TextGraphics.MarkerBehaviour
 
   @behaviour MarkerBehaviour
+
+  def get_points(%Board{} = board, {})
 end

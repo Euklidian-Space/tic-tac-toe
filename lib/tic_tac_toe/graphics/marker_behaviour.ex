11ac426d3f156a5546b8e7bb6257d833498caad9
data-kid <gabriel.grover@gmail.com>
2018-05-04 10:56:41 -0700

    made marker behaviour contract

--- lib/tic_tac_toe/graphics/marker_behaviour.ex
+++ lib/tic_tac_toe/graphics/marker_behaviour.ex
@@ -1,4 +1,9 @@
 defmodule TicTacToe.TextGraphics.MarkerBehaviour do
-  @callback get_points(%TicTacToe.Board{}, {non_neg_integer(), non_neg_integer()}) ::
-    [{non_neg_integer(), non_neg_integer()}]
+  @doc """
+  The tuples in the list of the first argument represent cells on the game board
+  string of the second argument.  The rows and columns that each cell occupies
+  are 1-indexed.
+  """
+  @callback place_marks([{non_neg_integer(), non_neg_integer()}], String.t())
+    :: {:ok, String.t()} | {:error, String.t()}
 end
